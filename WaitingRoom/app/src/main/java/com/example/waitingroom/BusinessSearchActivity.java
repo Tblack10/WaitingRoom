3355eecd4d0a2fd1eb737a71b5aaa9c56d29276a
Travis Black <Travis_Black@Live.com>
2020-10-08 13:58:51 -0700

    Layout created

--- WaitingRoom/app/src/main/java/com/example/waitingroom/BusinessSearchActivity.java
+++ WaitingRoom/app/src/main/java/com/example/waitingroom/BusinessSearchActivity.java
@@ -9,6 +9,6 @@ public class BusinessSearchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_business_search);
     }
 }
