c7a4594df526851fd47c47ab2fb0eabe1b11ad6d
Travis Black <Travis_Black@Live.com>
2020-10-08 13:18:03 -0700

    Added empty activities

---
+++ WaitingRoom/app/src/main/java/com/example/waitingroom/CallRequestActivity.java
@@ -0,0 +1,14 @@
+package com.example.waitingroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CallRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_request);
+    }
+}
---
+++ WaitingRoom/app/src/main/java/com/example/waitingroom/CallerDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.waitingroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CallerDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caller_detail);
+    }
+}
