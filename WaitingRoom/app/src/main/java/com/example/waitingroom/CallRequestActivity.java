74347215cc085eb8887d43ec41dee555c7359b89
Travis Black <Travis_Black@Live.com>
2020-10-19 23:50:00 -0700

    UI reconfigured, BusinessList passes businessName to callrequest

--- WaitingRoom/app/src/main/java/com/example/waitingroom/CallRequestActivity.java
+++ WaitingRoom/app/src/main/java/com/example/waitingroom/CallRequestActivity.java
@@ -3,12 +3,20 @@ package com.example.waitingroom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class CallRequestActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_request);
+
+        String bName = (String) getIntent().getExtras().get("businessName");
+        TextView businessNameTextView = findViewById(R.id.businessNameTextView);
+        businessNameTextView.setText(bName);
     }
 }
